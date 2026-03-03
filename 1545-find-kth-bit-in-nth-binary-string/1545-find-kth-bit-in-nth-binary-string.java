ac5008fb0f725d00902c972d9e95638eb597c07a
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-03-03 23:42:55 +0530

    Time: 61 ms (28.73%), Space: 47.3 MB (33.77%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        StringBuilder sb=new StringBuilder("0");
+        for(int i=1;i<n && k>sb.length();i++){
+            sb.append("1");
+            for(int j=sb.length()-2;j>=0;j--){
+                char invertbit=sb.charAt(j)=='1'?'0':'1';
+                sb.append(invertbit);
+            }
+        }
+        return sb.charAt(k-1);
+    }
+}
