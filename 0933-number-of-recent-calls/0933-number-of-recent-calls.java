b89b07bcb30ca08fe1f25a5fbf2887c811ad51d0
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-06-05 23:58:46 +0530

    Time: 28 ms (7.42%), Space: 59.2 MB (72.94%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,18 +1,12 @@
 class RecentCounter {
-    private static final int[] records=new int[10000];
-    private int start;
-    private int end;
+    Queue<Integer> q = new LinkedList<>();
     public RecentCounter() {
-        start=0;
-        end=0;
     }
     
     public int ping(int t) {
-        while(start < end && (t-records[start] > 3000)){
-            start++;
-        }
-        records[end++]=t;
-        return end - start;
+        q.offer(t);
+        while (q.peek() < t - 3000) q.poll(); // clear old
+        return q.size();
     }
 }
 
